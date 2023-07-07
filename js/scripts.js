07307f8c1593cce2c56a7db42170ce582ab895c2
ZRJul <mzr.juli@gmail.com>
2023-07-07 17:32:37 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+
+
+/*Є текстове поле на сторінці. При фокусі на цьому полі збоку з'являється <div> з інформацією. При зникненні фокуса - так само пропадає*/
+
+let textField = document.getElementById('text_field');
+let infoDiv = document.getElementById('info');
+
+textField.addEventListener('focus', function() {
+    infoDiv.style.display = 'block';
+});
+
+textField.addEventListener('blur', function() {
+    infoDiv.style.display = 'none';
+});
+
+/*На сторінці є дві кнопки. При натисканні на першу кнопку просимо користувача ввести в prompt посилання, при натисканні на другу - переадресовується на інший сайт (за раніше введеним посиланням). Реалізувати перевірку на http/https. Якщо протокол не вказано - додаємо*/
+
+
+
+/*Вивести таблицю 10 × 10, заповнену числами від 1 до 100 (таблиця створюється динамічно)*/
+
+
+/*У папці images є зображення 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5.jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg. Вивести зображення з цієї папки отримане випадковим чином (Math.random)
+*/

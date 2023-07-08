2f3e64ef19ca8ea4a7b0a7d4506d204b8ed45bbe
ZRJul <mzr.juli@gmail.com>
2023-07-08 13:49:01 +0300

    end1

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,7 @@ textField.addEventListener('blur', function() {
     infoDiv.style.display = 'none';
 });
 
+
 /*На сторінці є дві кнопки. При натисканні на першу кнопку просимо користувача ввести в prompt посилання, при натисканні на другу - переадресовується на інший сайт (за раніше введеним посиланням). Реалізувати перевірку на http/https. Якщо протокол не вказано - додаємо*/
 
 let goLink = document.getElementById('button_two')
